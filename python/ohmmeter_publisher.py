544d111ae3df2cc8cf44a9c6f66a014c7ff6a05a
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2024-01-22 23:59:41 +0900

    add temporary publisher code

---
+++ python/ohmmeter_publisher.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import rospy
+from hioki_ohmmeter.msg import OhmMeterData
+from ohmmeter import OhmMeter
+import time
+import sys
+
+class OhmMeterPublisher:
+    """
+    The class to publish the output of HIOKI RM3545 
+    """
+
+    def __init__( self, topicname, serialport, baudrate = 119200, timeout = 0.01, nodename = "OhmMeter"):
+        """
+        Constructor
+
+        引数
+            topicname: topic name to be published
+            serialport: the path to device file of RM3545 (e.g. "/dev/ttyACM0")
+            baudrate: baudrate of serial communication (default: 119200)
+            timeout: timeout of serial communication (default: 0.01秒)
+            nodename: node's name (Default: OhmMeter)
+        """
+        self.ohmmeter = OhmMeter( serialport, baudrate=baudrate, timeout=timeout)
+
+        rospy.init_node( nodename )
+        self.publisher = rospy.Publisher( topicname, OhmMeterData, queue_size=10 )
+
+    def __del__( self):
+        """
+        """
+        #self.ohmmeter.__del__()
+        del self.ohmmeter
+
+    def publish(self):
+        """
+        publish
+        """
+        publishdata = OhmMeterData()
+        publishdata.data = self.ohmmeter.read()
+        publishdata.header.stamp = rospy.Time.now()
+        self.publisher.publish(publishdata)
