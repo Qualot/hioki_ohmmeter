ce72a64e157ecb66c65faf2f06e685d27c959763
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2024-02-12 19:21:23 +0900

    modify symbolic link to ohmmeter0

--- scripts/ohmmeter_udev_setup.sh
+++ scripts/ohmmeter_udev_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "#udev id for ohmmeter
-SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"108f\", ATTRS{idProduct}==\"0001\", SYMLINK+=\"ohmmeter\"" > /tmp/72-ohmmeter-udev.rules
+SUBSYSTEM==\"tty\", ATTRS{idVendor}==\"108f\", ATTRS{idProduct}==\"0001\", SYMLINK+=\"ohmmeter0\"" > /tmp/72-ohmmeter-udev.rules
 
 sudo sh -c "cat /tmp/72-ohmmeter-udev.rules > /etc/udev/rules.d/72-ohmmeter-udev.rules"
 
