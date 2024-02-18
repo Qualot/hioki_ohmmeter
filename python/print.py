b133856ee0976911c1e4c5dded58ab19f74b7819
Qualot <nakashima@robot.t.u-tokyo.ac.jp>
2024-02-18 23:45:31 +0900

    modify print.py to use absl instead of argc, argv (print.py is not called from .launch file, so this modification might be unnecessary

--- python/print.py
+++ python/print.py
@@ -3,37 +3,35 @@
 
 import serial
 import time
-import sys
+from absl import app
+from absl import flags
 
 from ohmmeter import OhmMeter
 
 Timeout_default = 1
 
-if __name__=="__main__":
+FLAGS = flags.FLAGS
+flags.DEFINE_string("serial_port", "/dev/ohmmeter0", "Serial port: (default) /dev/ohmmeter0")
+flags.DEFINE_integer("print_rate", 10, "Print rate in Hz: (default) 10")
 
-    if len( sys.argv ) < 2:
-        print("print ohmmeter data")
-        print("usage:program <serial port> <print rate (optional default: 10Hz)>")
+def main(argv):
+    if FLAGS.serial_port is None:
+        print("Serial port not provided. Usage: program --serial_port=<serial port> [--print_rate=<print rate>]")
         sys.exit(1)
 
-    a = OhmMeter( sys.argv[1] )
-    if len( sys.argv ) > 2:
-        try:
-            rate = int(sys.argv[2])
-        except:
-            rate = 10
-    else:
-        rate = 10
+    a = OhmMeter(FLAGS.serial_port)
+    rate = FLAGS.print_rate
     dt = 1.0 / rate
-
 
     a.start_measure()
 
     try:
         while True:
-            #print( str(a.SendQueryMsg(":FETCh?", Timeout_default)))
-            print( float(a.read(Timeout_default)))
+            print(float(a.read(Timeout_default)))
             time.sleep(dt)
     except KeyboardInterrupt:
         a.stop_measure()
         del a
+
+if __name__ == "__main__":
+    app.run(main)
